5f79df1fbf44b411bcda1fed3c267a2f1d637f4f
Ahad <ahad.ahmad-khan@capgemini.com>
2025-02-10 05:39:04 +0530

    added home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,11 +16,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        if (User.Identity?.IsAuthenticated != true)
-        {
-            return RedirectToAction("Login", "Account");
-        }
-        return RedirectToAction("Index", "Car");
+        return View();
     }
 
     public IActionResult Privacy()
